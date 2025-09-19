00624c564a4c94e289fea3cf86760b668c6f233d
Gabrielly <splendoregabrielly222@gmail.com>
2025-09-19 11:51:54 -0300

    terminando

--- gerenciador_web/config/tarefas/views.py
+++ gerenciador_web/config/tarefas/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
-
-# Create your views here.
 from .models import Tarefa
-from django.http import HttpResponse
 
-def listar_tarefas (request):
+# from .models import Tarefa
+# from django.http import HttpResponse
+
+# def listar_tarefas (request):
+#     tarefas_salvas = Tarefa.objects.all()
+#     print (tarefas_salvas)
+#     return HttpResponse ("View 'listar_tarefas' executar! Confira no terminal")
+
+def listar_tarefas(request):
+    # 1: A busca no banco de dados continua a mesma
     tarefas_salvas = Tarefa.objects.all()
-    print (tarefas_salvas)
-    return HttpResponse ("View 'listar_tarefas' executar! Confira no terminal")
+
+    # 2. criamos um "dicionario de contexto" para enviar os dados ao template.
+    # A chave 'minhas_tarefas' sera a variavel que usaremos no HTML.
+    contexto = {
+        'minhas_tarefas': tarefas_salvas
+    }
+
+    # 3. Renderizamos o template, passando a requisiçao e o contexto com os dados.
+    return render(request, 'tarefas/lista.html', contexto)
