a8734b1bf50d0db05ced153fd267911846c4e653
Gabrielly <splendoregabrielly222@gmail.com>
2025-10-08 11:57:06 -0300

    py

--- gerenciador_web/config/tarefas/urls.py
+++ gerenciador_web/config/tarefas/urls.py
@@ -4,4 +4,7 @@ from . import views # importa nossas views
 urlpatterns = [
     path ('', views.listar_tarefas, name = 'lista_tarefas'),
     path ( '<int:tarefa_id>', views.detalhe_tarefa, name = "detalhe_tarefa"),
+
+    # Adicionar Tarefa
+    path('adicionar/', views.adicionar_tarefa, name = 'adicionar_tarefa'),
 ]
